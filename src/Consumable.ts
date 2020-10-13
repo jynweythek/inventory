4b5596c4087362eb739fde08bab56dbbf4c22181
Rustam_Talapov <rustam_talapov@epam.com>
2020-10-13 22:41:37 +0300

    5. Consumable class implementation

--- src/Consumable.ts
+++ src/Consumable.ts
@@ -1,1 +1,32 @@
-// your code goes here
+export default class Consumable {
+    name: string;
+    consumed: boolean;
+    spoiled: boolean;
+
+    constructor(name: string, consumed: boolean = false, spoiled: boolean) {
+        this.name = name;
+        this.consumed = consumed;
+        this.spoiled = spoiled;
+    }
+
+    use() {
+        if (!this.consumed && !this.spoiled) {
+            return this.eat();
+        }
+    }
+    eat() {
+        const DFLT_MESSAGE: string = `You eat the ${this.name}.`;
+        const SICK_MESSAGE: string = 'You feel sick.';
+
+        if (this.consumed) {
+            return `There is nothing left of the ${this.name} to consume.`;
+        } else {
+            if (this.spoiled) {
+                return `${DFLT_MESSAGE}\n${SICK_MESSAGE}`;
+            } else{
+                return `${DFLT_MESSAGE}`;
+            }
+        }
+    }
+}
+
